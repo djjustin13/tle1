8914ffaa24a57fa1856dac733cb927af57f57c00
Frank Solleveld <fsolleveld@outlook.com>
2018-11-15 16:21:41 +0100

    Added migrations for the db

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            // Users are not obliged to enter their zip code or house number if they don't want to. -FS
+            $table->string('zip')->nullable();
+            $table->string('housenumber')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
