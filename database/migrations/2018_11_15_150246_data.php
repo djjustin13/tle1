a7430a648bfa1bc244a1c5f62ff228aba53bb486
Frank Solleveld <fsolleveld@outlook.com>
2018-11-20 14:23:50 +0100

    Added row in data table -> data migration

--- database/migrations/2018_11_15_150246_data.php
+++ database/migrations/2018_11_15_150246_data.php
@@ -16,7 +16,10 @@ class Data extends Migration
         Schema::create('data', function (Blueprint $table) {
             $table->increments('id');
             $table->string('data_name');
-            $table->string('carbondioxide');
+            // CO2 export per year average
+            $table->integer('co2_year_average');
+            // C02 per km, minute, day average
+            $table->integer('co2_by_unit');
             $table->timestamps();
         });
     }
