b39f9594d4a69e4196e158bf800ce829b11fb400
Frank Solleveld <fsolleveld@outlook.com>
2018-11-20 15:18:02 +0100

    Added API Routes

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::resource('/data', 'DataController');
 
-Route::get();
+Route::get('/api/calculate/{allScoreData}', 'DataController@calculateAllScores');
+Route::get('/api/compare/{category}/{score}', 'DataController@compareScore');
+Route::get('/api/challenge/{challenge}/{option}' , 'DataController@setChallenge');
