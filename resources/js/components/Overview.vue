ecfeab191b473ccd4fc45dd15490796bde2696bf
Justin de Ruiter <djjustin13@gmail.com>
2018-12-11 16:33:26 +0100

    Added house to overview and added image maps

--- resources/js/components/Overview.vue
+++ resources/js/components/Overview.vue
@@ -1,24 +1,39 @@
 <template>
     <div>
-        <p>Overview</p>
-        {{ answers }}
-        <br>
-        <a @click="reset()" href="#">Clear storage & restart</a>
-        <br>
-        <button @click="$router.push('challengecar')" class="btn btn-dark">Car Challenge!</button>
-        <br>
-        <button @click="$router.push('challengemeat')" class="btn btn-dark">Meat Challenge!</button>
-        <br>
-        <button @click="$router.push('challengeshower')" class="btn btn-dark">Shower Challenge!</button>
-        <br>
-        <button @click="$router.push('challengesmoking')" class="btn btn-dark">Smoking Challenge!</button>
+        <div class="container">
+            <div v-if="error" class="alert alert-light" role="alert">
+                {{ error }}
+            </div>
 
+            <div class="row justify-content-center">
+                <div class="col-12 col-sm-8 col-md-6 col-lg-4 text-center">
+                    <img class="house-img" src="/img/house.png" alt="house" usemap="#image-map">
+                    <map name="image-map">
+                        <area target="" alt="Room 1" title="Room 1" href="#room1" coords="178,79,281,94,280,148,178,149" shape="poly">
+                        <area target="" alt="Room 2" title="Room 2" href="#room2" coords="286,95,342,105,341,148,285,149" shape="poly">
+                        <area target="" alt="Meat" title="Meat" href="#meat" coords="178,162,281,161,281,243,178,271" shape="poly">
+                        <area target="" alt="Room 4" title="Room 4" href="#room4" coords="285,161,342,157,343,225,285,241" shape="poly">
+                        <area target="" alt="Garage" title="Garage" href="#garage" coords="0,156,47,156,48,238,0,221" shape="poly">
+                        <area target="" alt="Solar panel" title="Solar panel" href="#roof" coords="100,16,290,53,344,100,176,76" shape="poly">
+                    </map>
+                    <hr>
+                    <p>Overview</p>
+                    <hr>
+                    {{ answers }}
+                    <br>
+                    <a @click="reset()" href="#">Clear storage & restart</a>
+                    <br>
+                    <button @click="$router.push('challengecar')" class="btn btn-dark">Car Challenge!</button>
+                    <br>
+                    <button @click="$router.push('challengemeat')" class="btn btn-dark">Meat Challenge!</button>
+                    <br>
+                    <button @click="$router.push('challengeshower')" class="btn btn-dark">Shower Challenge!</button>
+                    <br>
+                    <button @click="$router.push('challengesmoking')" class="btn btn-dark">Smoking Challenge!</button>
 
-        <!--<a href="/challengecar" class="btn btn-light ">Car Challenge!</a>-->
-        <!--<a href="/challengemeat" class="btn ">Meat Challenge!</a>-->
-        <!--<a href="/challengeshower" class="btn ">Shower Challenge!</a>-->
-        <!--<a href="/challengesmoking" class="btn ">Smoking Challenge!</a>-->
-
+                </div>
+            </div>
+        </div>
     </div>
 
 </template>
@@ -61,3 +76,12 @@
         }
     }
 </script>
+
+<style lang="scss" scoped>
+    .house-img{
+        width: 100%;
+    }
+
+</style>
+
+
